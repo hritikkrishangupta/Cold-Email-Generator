52feb2e0ecaee3c8ad755d410c0d3c08024758dd
Hritik Krishan Gupta <hritikkrishangupta@gmail.com>
2024-10-15 14:30:31 +0530

    Add files via upload

---
+++ app/chains.py
@@ -0,0 +1,55 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Hritik Krishan Gupta, a Software Developer specializing in AI/ML solutions. You have extensive experience in developing and deploying machine learning models and AI-powered applications that enhance business processes, drive innovation, and deliver scalable and efficient solutions.
+            Your role is to write a cold email to the client regarding the job mentioned above, highlighting your expertise in AI/ML and how you can address their specific needs with cutting-edge technology and tailored solutions.
+            Mention relevant projects from your portfolio, focusing on those that best align with the client’s requirements. Demonstrate your ability to create impactful, data-driven solutions that lead to process optimization and business growth.
+
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
